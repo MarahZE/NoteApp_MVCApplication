d1e151e6b7e314f8a460a8ae049ecc519244a536
maze6343 <maze6343@student.su.se>
2024-07-21 10:34:22 +0200

    save changes

--- NoteApp/obj/Debug/net8.0/NoteApp.AssemblyInfo.cs
+++ NoteApp/obj/Debug/net8.0/NoteApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd6d4edc8648074cbd8f44e2df50d66424b85cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a67bd62282dfa646aaa90ea6aedadbf6a149f9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
